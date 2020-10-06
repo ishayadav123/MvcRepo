0fc54262eb59ec82b77f4ed33d51633c1e5cef95
test <test@V-5051-DT085.vinove.com>
2020-10-06 17:55:38 +0530

    first commit

---
+++ MVCrepo2/Models/InsertViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCrepo2.Models
+{
+    public class InsertViewModel
+    {
+        public InsertViewModel()
+        {
+            UserDetailsData = new List<UserDetailsNew>();
+            CarLicenseDetails = new List<CarDetails>();
+        }
+
+       public List<UserDetailsNew> UserDetailsData { get; set; }
+       public List<CarDetails> CarLicenseDetails { get; set; }
+    }
+    public class UserDetailsNew 
+    { 
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string UserEmail { get; set; }
+        public string PasswordHash { get; set; }
+        public string CivilIdNumber { get; set; }
+        public string DOB { get; set; }
+        public string MobileNo { get; set; }
+        public string Address { get; set; }
+        public int? RoleId { get; set; }
+        public string ProfilePic { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool? IsNotificationActive { get; set; }
+        public bool? IsActive { get; set; }
+
+    }
+
+    public class CarDetails
+    {
+        public string CarLicense { get; set; }
+        public int CarId { get; set; }
+    }
+}
---
+++ MVCrepo2/repository/Itrainees.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using MVCrepo2.Models;
+
+
+
+namespace MVCrepo2.repository
+
+{
+
+    interface Itrainees
+
+    {
+
+        void InsertTrainees(UserDetail user); 
+
+        IEnumerable<UserDetail> GetTrainees();
+
+
+        UserDetail GetTraineesByID(int UserId);
+
+        UserDetail UpdateTrainees(UserDetail userDetail);
+
+        UserDetail DeleteTrainees(int UserId);
+
+        //object UpdateTrainees(int userId);
+    }
+
+}
